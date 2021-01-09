7e89d9927ea7a75ede555885b88a14b185bdc8b7
Rohit <rohitcricbuzz@gmail.com>
2021-01-09 00:02:18 +0000

    sum of numbers of array within arrays

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-function sumItems(array) {
+function sumItems(array, flattened = []) {
   // Sum all the numbers in the array
+  for (let i = 0; i < array.length; i++) {
+    if (!Array.isArray(array[i])) {
+      flattened.push(array[i]);
+    } else {
+      sumItems(array[i], flattened);
+    }
+  }
+  return flattened.reduce((cv,item) => cv + item);
 }
 
 module.exports = sumItems;
